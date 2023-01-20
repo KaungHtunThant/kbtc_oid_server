6f2e6c57e317513d0ae3e2734332aa7aa4cebbb1
Myozayarthant <98870195+Myozayarthant@users.noreply.github.com>
2023-01-20 16:29:45 +0630

    create students database

--- backend.php
+++ backend.php
@@ -107,4 +107,114 @@
 		$JSON = json_encode($json);
 		echo $JSON;
 	}
+//student Create
+	if(isset($_POST["studCreate"])){
+		$url = htmlspecialchars($_POST['url']);
+		$studID = htmlspecialchars($_POST['studID']);
+		$studName = htmlspecialchars($_POST['studName']);
+		$studBatch = htmlspecialchars($_POST['studBatch']);
+		$studClass = htmlspecialchars($_POST['studClass']);
+		$studguardianName = htmlspecialchars($_POST['studguardianName']);
+		$studdateofBirth = htmlspecialchars($_POST['studdateofBirth']);
+		$studemergencyPhone1 = htmlspecialchars($_POST['studemergencyPhone1']);
+		$studemergencyPhone2 = htmlspecialchars($_POST['studemergencyPhone2']);
+		$studschoolPhone = htmlspecialchars($_POST['studschoolPhone']);
+		$studKey = "RandomGiberishString";
+
+		$sql = "SELECT * FROM students WHERE studID = $studID";
+		$conn = dbconnect();
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+			$Message = "Error: ID Already Exists";
+		} else {
+			$sql = "INSERT INTO students(studID, studName, batchID, classID, guardianName, birthDate, studPhone1, studPhone2, schoolPhone, studStatus, studKey) VALUES('$studID', '$studName', '$studBatch', '$studClass', '$studguardianName','$studdateofBirth','$studemergencyPhone1','$studemergencyPhone2','$studschoolPhone', 1, '$studKey')";
+			if ($conn->query($sql) === TRUE) {
+				$Message = "Success";
+			} else {
+				$Message = "Error: " . $sql . "<br>" . $conn->error;
+			}
+		}
+		$conn->close();
+		$json = array(
+			"Message" => $Message
+		);
+		$JSON=json_encode($json);
+		echo $JSON;
+	}
+
+	//student Read
+	if(isset($_POST["studRead"])){
+		$json = array();
+		$sql = "SELECT * FROM students WHERE studStatus = 1";
+
+		$conn = dbconnect();
+		$result = $conn->query($sql);
+		$i = 0;
+
+		//Prepare convertion to Json
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				$arr = array(
+					"studID" => $row["studID"],
+					"studName" => $row["studName"],
+					"studBatch" => $row["batchID"],
+					"studClass" => $row["classID"],
+					"studqrcode" => "https://localhost/kbtc_oid_client/assets/qrcodes/student/".$id.".png",
+					"studKey" => $row["studKey"]
+				);
+				$json["row$i"] = $arr;
+				$i += 1;
+			}
+			$Message = "Read Success!";
+		} else {
+			$Message = "Error: " . $sql . "<br>" . $conn->error;
+		}
+		$json["ReadMessage"] = $Message;
+		$json["ReadCount"] = $i;
+
+		//Encode Array to Json
+		$JSON = json_encode($json);
+		echo $JSON;
+	}
+
+	if(isset($_POST["studReadDetails"])){
+		$json = array();
+		$empID = htmlspecialchars($_POST['studID']);
+		$sql = "SELECT * FROM students WHERE studStatus = 1 AND studID='$empID';";
+
+		$conn = dbconnect();
+		$result = $conn->query($sql);
+		$i = 0;
+
+		//Prepare convertion to Json
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				$arr = array(
+					"studID" => $row["studID"],
+					"studName" => $row["studName"],
+					"studBatch" => $row["studBatch"],
+					"empPositionID" => $row["empPositionID"],
+					"empDeptID" => $row["empDeptID"],
+					"empJoinDate" => $row["empJoinDate"],
+					"empqrcode" => "https://localhost/kbtc_oid_client/assets/qrcodes/".$id.".png",
+					"empKey" => $row["empKey"]
+				);
+				$json["row$i"] = $arr;
+				$i += 1;
+			}
+			$Message = "Read Success!";
+		} else {
+			$Message = "Error: " . $sql . "<br>" . $conn->error;
+		}
+		$json["ReadMessage"] = $Message;
+		$json["ReadCount"] = $i;
+
+		//Encode Array to Json
+		$JSON = json_encode($json);
+		echo $JSON;
+	}
+	
+	
+
+
 ?>
